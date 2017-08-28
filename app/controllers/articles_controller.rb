c8ecb92bbfc4a43dff9735908e289429650ec7d9
snayrouz <samuel.nayrouz@gmail.com>
2017-08-28 12:32:38 -0600

    Add article view and index action

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+
+    def index
+      @articles = Article.all
+    end
+
+    def create
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+end
